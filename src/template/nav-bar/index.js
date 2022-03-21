f1ef2fa88522b14576589aa8e1d8935702bc3616
Pushp Singh <pushp_singh@condenast.com>
2022-03-21 18:15:49 +0530

    implemented rest listing page

--- src/template/nav-bar/index.js
+++ src/template/nav-bar/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import logo from '../../image/logo-horizontal.svg';
+import './nav-bar.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NavBar Page here</h1>
-            <Link to='/about-us'>About Us</Link>
-            <br />
-            <Link to='/listing'>List Page</Link>
-        </div>
+        <Grid className="navbar-container" container direction="row" justifyContent="space-between" alignItems="center">
+            <Grid className="logo" item xs={3}>
+                <span>icon</span>
+                <span>deliveroo</span>
+                {/* <Avatar className="logo-avatar" alt="Deliveroo" src={logo} /> */}
+            </Grid>
+            <Grid className="username" item xs={3}>
+                {/* <Link to='/listing'>List Page</Link> */}
+                <span>
+                 icon
+                </span>
+                <span>Pushp Singh</span>
+            </Grid>
+        </Grid>
     )
 }
 
