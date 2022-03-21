a1b12adacb6879cf9d19e00b1037656aac4cb1c0
Pushp Singh <pushp_singh@condenast.com>
2022-03-21 23:23:53 +0530

    finished work

--- src/template/nav-bar/index.js
+++ src/template/nav-bar/index.js
@@ -4,21 +4,20 @@ import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import logo from '../../image/logo-horizontal.svg';
+import avatar from '../../image/user-icon.svg';
 import './nav-bar.css';
 
 const NavBar = () => {
     return (
         <Grid className="navbar-container" container direction="row" justifyContent="space-between" alignItems="center">
             <Grid className="logo" item xs={3}>
-                <span>icon</span>
-                <span>deliveroo</span>
-                {/* <Avatar className="logo-avatar" alt="Deliveroo" src={logo} /> */}
+                {/* <span>icon</span>
+                <span>deliveroo</span> */}
+                <Avatar className="logo-avatar" alt="Deliveroo" src={logo} />
             </Grid>
             <Grid className="username" item xs={3}>
                 {/* <Link to='/listing'>List Page</Link> */}
-                <span>
-                 icon
-                </span>
+                <Avatar className="user-icon" alt="avatar" src={avatar} />
                 <span>Pushp Singh</span>
             </Grid>
         </Grid>
